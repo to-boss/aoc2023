bd674bc1b505cdebe6515b7e3a1011ab6e753338
Tobias Bossert <tobilan@live.de>
2023-12-25 13:56:43 +0100

    day7 cleanup

--- src/day07.rs
+++ src/day07.rs
@@ -90,6 +90,19 @@ enum HandType {
 }
 
 impl HandType {
+    fn from_hash_map(hash_map: &HashMap<char, i32>) -> Self {
+        match hash_map.len() {
+            1 => HandType::Fives,
+            2 if hash_map.iter().any(|(_, &v)| v == 4) => HandType::Fours,
+            2 => HandType::FullHouse,
+            3 if hash_map.iter().any(|(_, &v)| v == 3) => HandType::Threes,
+            3 => HandType::TwoPair,
+            4 => HandType::OnePair,
+            5 => HandType::HighCard,
+            _ => unreachable!("Cant create HandType from HashMap {:?}", hash_map),
+        }
+    }
+
     fn from_str_with_joker(s: &str) -> Self {
         let mut hash_map = HashMap::new();
         let mut joker_count = 0;
@@ -116,16 +129,7 @@ impl HandType {
             .entry(*highest_char)
             .and_modify(|e| *e += joker_count);
 
-        match hash_map.len() {
-            1 => HandType::Fives,
-            2 if hash_map.iter().any(|(_, &v)| v == 4) => HandType::Fours,
-            2 => HandType::FullHouse,
-            3 if hash_map.iter().any(|(_, &v)| v == 3) => HandType::Threes,
-            3 => HandType::TwoPair,
-            4 => HandType::OnePair,
-            5 => HandType::HighCard,
-            _ => unreachable!("no clue which HandType for: {}", s),
-        }
+        HandType::from_hash_map(&hash_map)
     }
 }
 
@@ -138,40 +142,45 @@ impl FromStr for HandType {
             hash_map.entry(c).and_modify(|e| *e += 1).or_insert(1);
         }
 
-        let hand_type = match hash_map.len() {
-            1 => HandType::Fives,
-            2 if hash_map.iter().any(|(_, &v)| v == 4) => HandType::Fours,
-            2 => HandType::FullHouse,
-            3 if hash_map.iter().any(|(_, &v)| v == 3) => HandType::Threes,
-            3 => HandType::TwoPair,
-            4 => HandType::OnePair,
-            5 => HandType::HighCard,
-            _ => unreachable!("no clue which HandType for: {}", s),
-        };
-
+        let hand_type = HandType::from_hash_map(&hash_map);
         Ok(hand_type)
     }
 }
 
 #[derive(Debug)]
 struct Hand {
-    hand: String,
+    cards: String,
     hand_type: HandType,
     bid: usize,
 }
 
 impl Hand {
     fn from_str_with_joker(s: &str) -> anyhow::Result<Self> {
-        let (hand, bid) = s.split_once(" ").unwrap();
-        let hand_type = HandType::from_str_with_joker(hand);
-        let hand = hand.to_string();
+        let (cards, bid) = s.split_once(" ").unwrap();
+        let hand_type = HandType::from_str_with_joker(cards);
+        let cards = cards.to_string();
         let bid = bid.parse().unwrap();
 
         Ok(Hand {
-            hand,
+            cards,
             hand_type,
             bid,
         })
+    }
+
+    fn compare_equal_hands(&self, other_hand: &Hand, index: usize, with_joker: bool) -> Ordering {
+        let a_char = self.cards.chars().nth(index).unwrap();
+        let b_char = other_hand.cards.chars().nth(index).unwrap();
+
+        if with_joker {
+            let a = CardValueWithJoker::from(a_char);
+            let b = CardValueWithJoker::from(b_char);
+            a.partial_cmp(&b).unwrap()
+        } else {
+            let a = CardValue::from(a_char);
+            let b = CardValue::from(b_char);
+            a.partial_cmp(&b).unwrap()
+        }
     }
 }
 
@@ -185,31 +194,11 @@ impl FromStr for Hand {
         let bid = bid.parse().unwrap();
 
         Ok(Hand {
-            hand,
+            cards: hand,
             hand_type,
             bid,
         })
     }
-}
-
-fn compare_equal_hands_with_joker(a: &str, b: &str, index: usize) -> Ordering {
-    let a_char = a.chars().nth(index).unwrap();
-    let b_char = b.chars().nth(index).unwrap();
-
-    let a = CardValueWithJoker::from(a_char);
-    let b = CardValueWithJoker::from(b_char);
-
-    a.partial_cmp(&b).unwrap()
-}
-
-fn compare_equal_hands(a: &str, b: &str, index: usize) -> Ordering {
-    let a_char = a.chars().nth(index).unwrap();
-    let b_char = b.chars().nth(index).unwrap();
-
-    let a = CardValue::from(a_char);
-    let b = CardValue::from(b_char);
-
-    a.partial_cmp(&b).unwrap()
 }
 
 fn solve2(s: &str) -> usize {
@@ -222,18 +211,11 @@ fn solve2(s: &str) -> usize {
         let mut ord = b.hand_type.partial_cmp(&a.hand_type).unwrap();
         let mut index = 0;
         while let Ordering::Equal = ord {
-            ord = compare_equal_hands_with_joker(&a.hand, &b.hand, index);
+            ord = a.compare_equal_hands(b, index, true);
             index += 1;
         }
         ord
     });
-
-    /*
-    for (rank, hand) in hands.iter().enumerate() {
-        let rank = rank + 1;
-        println!("{} {:?}, {rank} * {}", hand.hand, hand.hand_type, hand.bid);
-    }
-    */
 
     hands
         .iter()
@@ -252,18 +234,11 @@ fn solve1(s: &str) -> usize {
         let mut ord = b.hand_type.partial_cmp(&a.hand_type).unwrap();
         let mut index = 0;
         while let Ordering::Equal = ord {
-            ord = compare_equal_hands(&a.hand, &b.hand, index);
+            ord = a.compare_equal_hands(b, index, false);
             index += 1;
         }
         ord
     });
-
-    /*
-    for (rank, hand) in hands.iter().enumerate() {
-        let rank = rank + 1;
-        // println!("{} {:?}, {rank} * {}", hand.hand, hand.hand_type, hand.bid);
-    }
-    */
 
     hands
         .iter()
